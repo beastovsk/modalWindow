f5860dedd14da7075a51db24b1bcc4787c177ce9
Artyom <narshinovartyom@mail.ru>
2021-11-17 11:56:09 +0400

    ready to use

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const modal = $.modal({
     title: 'Some title',
     content: 'Some content',
-    width: '600px'
+    width: '600px',
+    footerButtons: [
+        {text: 'Ok', type: 'secondary', handler() {
+            console.log('Ok')
+        }},
+        {text: 'Cancel', type: 'secondary', handler() {
+            console.log('Cancel')
+            modal.close()
+        }}
+    ]
 })
 
 
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,5 +1,34 @@
+Element.prototype.appendAfter = function(elem) {
+    elem.parentNode.insertBefore(this, elem.nextSibling)
+}
+
+function nope() {}
+
+function _createFooter(buttons = []) {
+    if (buttons.length == 0) {
+        return document.createElement('div')
+    }
+
+    const wrap = document.createElement('div')
+    wrap.classList.add('modal-footer')
+    
+    buttons.forEach((btn) => {
+        const $btn = document.createElement('button')
+        $btn.textContent = btn.text
+        $btn.classList.add('btn')
+        $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+        $btn.onclick = btn.handler || nope
+
+        wrap.appendChild($btn)
+    })
+
+    return wrap
+}
+
+
 function _createModal(options) {
     const modal = document.createElement('div')
+    
 
     modal.classList.add('myModal')
     modal.insertAdjacentHTML('afterbegin', `
@@ -9,16 +38,16 @@ function _createModal(options) {
                     <span>${options.title}</span>
                     <span class="closeBtn" data-close="true">&times;</span>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body" data-content>
                     ${options.content || ''}
-                </div>
-                <div class="modal-footer">
-                    <button class="acceptBtn">Ok</button>
-                    <button class="cancelBtn">Cancel</button>
                 </div>
             </div>
         </div>
     `)
+
+    const footer = _createFooter(options.footerButtons)
+    footer.appendAfter(modal.querySelector('[data-content]'))
+
 
     document.body.appendChild(modal)
     return modal
@@ -62,6 +91,9 @@ $.modal = function(options) {
             $modal.parentNode.removeChild($modal)
             $modal.removeEventListener('click', listener)
             destroyed = true
+        }, 
+        setContent(html) {
+            $modal.querySelector('[data-content]').innerHTML = html
         }
     })
 }
